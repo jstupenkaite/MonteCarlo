9323360ba5486020163c41d256c6b8ee84f9d083
Justina Stupenkaite <st.justina.st@gmail.com>
2012-10-09 19:40:42 +0200

    papildziau vektoriu generavimu

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    cout << "MonteCarlo realizacijos pradzia" << endl;
-    cout << "(RANDOM SEARCH)" << endl;
-    // Kintamuju apsirasymas
-    double f, x; // f-jos reiksme ir argumentas x
-    int n; // uzdavinio dimensija
-    int xspr; // Argumento
-    int fspr; // uzdavinio sprendinys
-    return 0;
+  // n = 1;
+  int n;
+  double num2[10];
+  cout << "n = 1" << endl;
+  double low, high;
+  cin >> low;
+  cin >> high;
+  double range=(high-low);
+  double num = rand() * range / RAND_MAX + low ;
+  cout << num << endl;
+  // n = 2
+  cout << "n = 2" << endl;
+  double low2, high2;
+  cin >> low2;
+  cin >> high2;
+  cout << "Kokio ilgio vektorius: ";
+  cin >> n;
+  double range2=(high2-low2);
+  for (int i = 1; i<=n; i++)
+  {
+    num2[i] = rand() * range / RAND_MAX + low2;
+    cout << num2[i] << endl;
+  }
+
 }
