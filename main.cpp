b14a691f6273d1b118782c6c4bff4dd18d5b4d35
Justina Stupenkaite <st.justina.st@gmail.com>
2012-10-09 18:38:53 +0200

    Pridejome xspr

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@ using namespace std;
 int main()
 {
     cout << "MonteCarlo realizacijos pradzia" << endl;
+    cout << "(RANDOM SEARCH)" << endl;
     // Kintamuju apsirasymas
     double f, x; // f-jos reiksme ir argumentas x
     int n; // uzdavinio dimensija
+    int xspr; // Argumento
 
     return 0;
 }
