e0493dbaf34436a1cbf1af900c4a6fe9f1df7c81
Justina Stupenkaite <st.justina.st@gmail.com>
2012-10-09 18:33:32 +0200

    Papildziau kintamuju aprasyma

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main()
     // Kintamuju apsirasymas
     double f, x; // f-jos reiksme ir argumentas x
     int n; // uzdavinio dimensija
-
+    int fspr; // uzdavinio sprendinys
     return 0;
 }
