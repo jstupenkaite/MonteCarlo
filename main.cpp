417c756efe01e986718dd59461c051cf3bb2f5ef
Justina Stupenkaite <st.justina.st@gmail.com>
2012-10-09 18:10:41 +0200

    Apsiraseme kintamuosius.

--- main.cpp
+++ main.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main()
 {
     cout << "MonteCarlo realizacijos pradzia" << endl;
+    // Kintamuju apsirasymas
+    double f, x; // f-jos reiksme ir argumentas x
+    int n; // uzdavinio dimensija
+
     return 0;
 }
